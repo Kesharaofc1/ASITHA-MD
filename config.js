67894b10d4ee04a28e5179dd0f7c33002bf213ea
Kesharaofc1 <145447902+Kesharaofc1@users.noreply.github.com>
2024-10-24 19:36:16 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=m6xCWA5a#sOYZ25k0UOSQY4Kc2-p-dTsqn-bjv6yc_k1b9smKmxI",
+SESSION_ID: process.env.SESSION_ID || "m6xCWA5a#sOYZ25k0UOSQY4Kc2-p-dTsqn-bjv6yc_k1b9smKmxI",
 MONGODB: process.env.MONGODB || "mongodb+srv://rush:20041025sa@cluster0.ysi6dsf.mongodb.net/",//enter mongo db url හදන විදිය පල්ලෙහාබටන් එකක් ඇති
 };
